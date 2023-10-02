6bf21a7b4cac91fd42f17bb82395283794bef081
sguillau <guillaume.serret@ynov.com>
2023-10-02 23:54:31 +0200

    drawhangman

--- DrawHangMan.go
+++ DrawHangMan.go
@@ -1,8 +1,14 @@
 package hangman_classic
 
+import "fmt"
+
 // files : "assets/Hangman Positions/hangman.txt"
 
 func DrawHangMang(data HangManData, state int) {
 	HangMan := ReadFile("assets/Hangman Positions/hangman.txt")
-	
+	for index, character := range string {
+		if index >= state*8 && index < (state+1)*8 {
+			fmt.Println(character)
+		}
+	}
 }
