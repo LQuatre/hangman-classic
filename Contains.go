544f8e7d901f2d6ae4b8a31ad30f83a6b131285b
DIOT Lucas <unmecpasfort@gmail.com>
2023-10-03 13:40:22 +0200

    change win message, change format game

--- Contains.go
+++ Contains.go
@@ -2,6 +2,12 @@ package hangman_classic
 
 func Contains(s string, e string) bool {
 	for _, a := range s {
+		if isLower(a) {
+			a = putUpper(a)
+		}
+		if isLower(rune(e[0])) {
+			e = string(putUpper(rune(e[0]))) + e[1:]
+		}
 		if string(a) == e {
 			return true
 		}
--- Play.go
+++ Play.go
@@ -12,13 +12,43 @@ type HangManData struct {
 	HangmanPositions [10]string // It can be the array where the positions parsed in "hangman.txt" are stored
 }
 
+func isUpper(r rune) bool {
+	return r >= 'A' && r <= 'Z'
+}
+
+func isLower(r rune) bool {
+	return r >= 'a' && r <= 'z'
+}
+
+func putUpper(s rune) rune {
+	if s == '_' {
+		return s
+	}
+	if isLower(s) {
+		return s - 32
+	}
+	return s
+}
+
+func putLower(s rune) rune {
+	if s == '_' {
+		return s
+	}
+	if isUpper(s) {
+		return s + 32
+	}
+	return s
+}
+
 func Play(data HangManData) {
 	// X Le programme lit l'entrée standard pour suggérer une lettre.
-	fmt.Println("Welcome to Hangman !")
-	fmt.Println("You have 10 attempts to guess the word.")
-	fmt.Println("The word has", data.WordLength, "letters.")
-	fmt.Println("The word is : ", data.Word)
-	fmt.Println("Please enter a letter : ")
+	fmt.Println("Good Luck, you have 10 attempts.")
+	for _, v := range data.Word {
+		fmt.Print(string(putUpper(v)) + " ")
+	}
+	fmt.Print("\n\n")
+
+	fmt.Print("Choose: ")
 
 	var read string
 	fmt.Scanln(&read)
@@ -28,33 +58,35 @@ func Play(data HangManData) {
 		// X Si la lettre n'est pas présente, il affichera un message d'erreur et le nombre de tentatives diminuera (10->9->...0).
 		if !Contains(data.ToFind, read) {
 			data.Attempts++
-
+			fmt.Println("Not present in the word, ", 10-data.Attempts, " attempts remaining.")
 			DrawHangMang(data.Attempts)
-
-			fmt.Println("The letter is not in the word. You have", data.Attempts, "attempts left.")
-			fmt.Println("The word is : ", data.Word)
 		} else {
 			// X Si la lettre est présente, il révélera toutes les lettres correspondantes dans le mot.
-			fmt.Println("The letter is in the word.")
 			for i := 0; i < data.WordLength; i++ {
-				if data.ToFind[i] == read[0] {
-					data.Word = data.Word[:i] + read + data.Word[i+1:]
+				guestUint8 := read[0]
+				guestRune := rune(guestUint8)
+				if putUpper(rune(data.ToFind[i])) == putUpper(guestRune) {
+					data.Word = data.Word[:i] + string(guestRune) + data.Word[i+1:]
 				}
 			}
-			fmt.Println("The word is : ", data.Word)
+			for _, v := range data.Word {
+				fmt.Print(string(putUpper(v)) + " ")
+			}
+			fmt.Print("\n\n")
 		}
 
 		if data.Attempts == 10 {
 			fmt.Println("You lose !")
+			fmt.Println("The word was: ", data.ToFind)
 			break
 		}
 
 		// X Le programme continue jusqu'à ce que le mot soit trouvé, ou que le nombre de tentatives soit égal à 0.
 		if Contains(data.Word, "_") {
-			fmt.Println("Please enter a letter : ")
+			fmt.Print("Choose: ")
 			fmt.Scanln(&read)
 		} else {
-			fmt.Println("You win !")
+			fmt.Println("Congrats !")
 			break
 		}
 	}
