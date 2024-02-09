522909c32d3b909d92c6d68c17f9d519f88eb1d1
LQuatre <83190245+LQuatre@users.noreply.github.com>
2024-02-09 03:37:21 +0100

    BECOME PRACTICABLE AND USABLE

--- core/Play.go
+++ core/Play.go
@@ -39,84 +39,130 @@ func Print(str string, data HangManData) {
 func isLower(char rune) bool {
 	return char >= 'a' && char <= 'z'
 }
+
 func putUpper(char rune) rune {
 	return char - ('a' - 'A')
 }
 
 func Play(data HangManData) {
+	// Afficher le mot initial
+	displayWord(data)
+
 	var info string
 	info += "Welcome to Hangman !\n"
+	info += "Good Luck, you have " + strconv.Itoa(10-data.Attempts) + " attempts remaining.\n"
+	print(info)
 
+	var read string
+	for data.Attempts < 10 {
+		// Vérifier si le jeu est terminé
+		if gameEnded(data) {
+			break
+		}
+
+		// Lire l'entrée du joueur
+		readInput(&read)
+
+		// Traiter l'entrée du joueur
+		processInput(&data, &info, read)
+
+		// Afficher le mot mis à jour et le pendu
+		displayWord(data)
+		DrawHangMang(data.Attempts)
+
+		// Afficher les informations
+		print(info)
+		info = ""
+	}
+	print("Game over ! The word was " + data.ToFind + ".\n")
+}
+
+func isLetterPresent(data HangManData, letter string) bool {
+	result, _ := Contains(data.ToFind, letter)
+	return result
+}
+
+func updateWord(data HangManData, letter string) {
+	for i := 0; i < data.WordLength; i++ {
+		if string(data.ToFind[i]) == letter {
+			data.Word = data.Word[:i] + letter + data.Word[i+1:]
+		}
+	}
+}
+
+// Fonction pour afficher le mot avec les lettres découvertes
+func displayWord(data HangManData) {
 	if data.IsAscii == true {
 		Print(data.Word, data)
 	} else {
 		fmt.Println(data.Word)
 	}
+}
 
-	info = "Good Luck, you have " + strconv.Itoa(10-data.Attempts) + " attempts remaining.\n"
+// Fonction pour vérifier si le jeu est terminé
+func gameEnded(data HangManData) bool {
+	if present, _ := Contains(data.Word, "_"); !present {
+		data.Word = data.ToFind
+		fmt.Println("Congrats !")
+		saveRank(data)
+		return true
+	}
+	return false
+}
 
-	DrawHangMang(data.Attempts)
-	print(info)
-	info = ""
+// Fonction pour lire l'entrée du joueur
+func readInput(read *string) {
+	fmt.Print("Choose: ")
+	fmt.Scanln(read)
+	ClearTerminal()
+}
 
-	var read string
-	for data.Attempts < 10 {
-		if present, _ := Contains(data.Word, "_"); present {
-			fmt.Print("Choose: ")
-			fmt.Scanln(&read)
-			ClearTerminal()
-		} else {
-			data.Word = data.ToFind
-			fmt.Println("Congrats !")
-			saveRank(data)
-			break
-		}
+// Fonction pour traiter l'entrée du joueur
+func processInput(data *HangManData, info *string, read string) {
+	if len(read) > 1 {
+		// Traiter l'entrée si elle est un mot
+		processWordInput(data, info, read)
+	} else {
+		// Traiter l'entrée si elle est une lettre
+		processLetterInput(data, info, read)
+	}
+}
 
-		if len(read) > 1 {
-			if read == "STOP" || read == "stop" || read == "Stop" || read == "QUIT" || read == "quit" || read == "Quit" {
-				info += "Game stopped.\n"
-				saveGame(data)
-				break
+// Fonction pour traiter l'entrée si elle est un mot
+func processWordInput(data *HangManData, info *string, read string) {
+	if read == "STOP" || read == "stop" || read == "Stop" || read == "QUIT" || read == "quit" || read == "Quit" {
+		*info += "Game stopped.\n"
+		saveGame(*data)
+		return
+	}
+
+	if read == data.ToFind {
+		data.Word = data.ToFind
+		*info += "Congrats !\n"
+		saveRank(*data)
+	} else {
+		data.Attempts += 2
+		*info += "Wrong word, " + strconv.Itoa(10-data.Attempts) + " attempts remaining.\n"
+	}
+}
+
+// Fonction pour traiter l'entrée si elle est une lettre
+func processLetterInput(data *HangManData, info *string, read string) {
+	if read == "" {
+		*info += "Please enter a valid letter.\n"
+		return
+	}
+
+	if !isLetterPresent(*data, read) {
+		data.Attempts++
+		*info += "Wrong letter, " + strconv.Itoa(10-data.Attempts) + " attempts remaining.\n"
+	} else {
+		updateWord(*data, read)
+		*info += "Present letter, " + strconv.Itoa(10-data.Attempts) + " attempts remaining.\n"
+		for i := 0; i < data.WordLength; i++ {
+			if string(data.ToFind[i]) == read {
+				data.Word = data.Word[:i] + read + data.Word[i+1:]
 			}
-
-			if read == data.ToFind {
-				data.Word = data.ToFind
-				info += "Congrats !\n"
-				saveRank(data)
-				break
-			} else {
-				data.Attempts += 2
-				info += "Wrong word, " + strconv.Itoa(10-data.Attempts) + " attempts remaining.\n"
-			}
-		} else {
-			if present, _ := Contains(data.ToFind, read); !present {
-				data.Attempts++
-				info += "Wrong letter, " + strconv.Itoa(10-data.Attempts) + " attempts remaining.\n"
-			} else if read == "" {
-				info += "Please enter a valid letter.\n"
-			} else {
-				for i := 0; i < data.WordLength; i++ {
-					if string(data.ToFind[i]) == read {
-						data.Word = data.Word[:i] + read + data.Word[i+1:]
-					}
-				}
-				info += "Present letter, " + strconv.Itoa(10-data.Attempts) + " attempts remaining.\n"
-			}
-		}
-
-		if data.IsAscii == true {
-			Print(data.Word, data)
-		} else {
-			fmt.Println(data.Word)
-		}
-		DrawHangMang(data.Attempts)
-		print(info)
-		info = ""
-
-		if data.Attempts == 10 {
-			fmt.Println("You lose !")
-			fmt.Println("The word was: ", data.ToFind)
-			break
 		}
 	}
 }
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package hangmanclassic
+package main
 
 import (
 	"fmt"
@@ -6,6 +6,113 @@ import (
 	"os"
 	"strings"
 )
+
+func main() {
+	core.ClearTerminal()
+	MyHangManData := core.HangManData{}
+	ranking := loadRanking()
+
+	fmt.Println("Do you want to see the ranking ? (y/n)")
+	answer := getValidAnswer()
+
+	if answer == "y" {
+		displayRanking(ranking)
+	}
+
+	fmt.Println("Do you want to continue ? (y/n)")
+	answer = getValidAnswer()
+	if answer != "y" {
+		return
+	}
+
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("Enter arguments")
+		return
+	}
+
+	if len(args) == 1 {
+		if core.Readable("assets/Dictionary/" + args[0]) {
+			core.NewGame(args, MyHangManData)
+		} else {
+			fmt.Println("Please provide a valid argument.")
+		}
+		return
+	}
+
+	if present, pos := core.ContainsArray(args, "--startWith"); present {
+		handleStartWith(args, pos, MyHangManData)
+		return
+	}
+
+	if present, pos := core.ContainsArray(args, "--letterFile"); present {
+		handleLetterFile(args, pos, MyHangManData)
+		return
+	}
+}
+
+func loadRanking() [][3]string {
+	rankingFile := core.ReadFile("assets/Save/ranking.txt")
+	var ranking [][3]string // PlayerName : Attempts left : Word
+	for _, line := range rankingFile {
+		if line != "" {
+			ranking = append(ranking, [3]string{strings.Split(line, ":")[0], strings.Split(line, ":")[1], strings.Split(line, ":")[2]})
+		}
+	}
+	return ranking
+}
+
+func getValidAnswer() string {
+	var answer string
+	fmt.Scanln(&answer)
+	for answer != "y" && answer != "n" {
+		fmt.Println("Please enter a valid answer.")
+		fmt.Scanln(&answer)
+	}
+	return answer
+}
+
+func displayRanking(ranking [][3]string) {
+	fmt.Println("Voici le classement :")
+	fmt.Println("Name : Attempts left : Word")
+	sortedRanking := sortRanking(ranking)
+	for i, player := range sortedRanking {
+		if i < 10 {
+			medal := ""
+			switch i {
+			case 0:
+				medal = "🥇 "
+			case 1:
+				medal = "🥈 "
+			case 2:
+				medal = "🥉 "
+			default:
+				medal = "🏅 "
+			}
+			fmt.Printf("%s%s with %s attempts left and the word %s\n", medal, player[0], player[1], player[2])
+		}
+	}
+}
+
+func sortRanking(ranking [][3]string) [][3]string {
+	var sortedRanking [][3]string // PlayerName : Attempts left : Word
+	for _, player := range ranking {
+		if len(sortedRanking) == 0 {
+			sortedRanking = append(sortedRanking, player)
+		} else {
+			for j, sortedPlayer := range sortedRanking {
+				if stringToInt(player[1]) > stringToInt(sortedPlayer[1]) {
+					sortedRanking = append(sortedRanking[:j], append([][3]string{player}, sortedRanking[j:]...)...)
+					break
+				} else if j == len(sortedRanking)-1 {
+					sortedRanking = append(sortedRanking, player)
+					break
+				}
+			}
+		}
+	}
+	return sortedRanking
+}
 
 func stringToInt(s string) int {
 	result := 0
@@ -15,101 +122,31 @@ func stringToInt(s string) int {
 	return result
 }
 
-func main() {
-	core.ClearTerminal()
-	MyHangManData := core.HangManData{}
-	args := os.Args[1:]
-	rankingFile := core.ReadFile("assets/Save/ranking.txt")
-	var ranking [][3]string // PlayerName : Attempts left : Word
-	for _, line := range rankingFile {
-		if line != "" {
-			ranking = append(ranking, [3]string{strings.Split(line, ":")[0], strings.Split(line, ":")[1], strings.Split(line, ":")[2]})
+func handleStartWith(args []string, pos int, MyHangManData core.HangManData) {
+	if core.Readable("assets/Save/" + args[pos+1]) {
+		saved := core.ReadFile("assets/Save/" + args[2])
+		if len(saved) == 0 || saved[0] == "" {
+			core.NewGame(args, MyHangManData)
+			return
 		}
+		MyHangManData.ToFind = saved[0]
+		MyHangManData.Word = saved[1]
+		MyHangManData.PlayerName = saved[2]
+		MyHangManData.WordLength = len(MyHangManData.Word)
+		MyHangManData.Attempts = stringToInt(saved[3])
+		core.Play(MyHangManData)
+		core.EmptyFile("assets/Save/" + args[2])
+	} else {
+		fmt.Println("Please provide a valid argument.")
 	}
-	fmt.Println("Do you want to see the ranking ? (y/n)")
-	var answer string
-	fmt.Scanln(&answer)
-	for answer != "y" && answer != "n" {
-		fmt.Println("Please enter a valid answer.")
-		fmt.Scanln(&answer)
-	}
-	if answer == "y" {
-		fmt.Println("Voici le classement :")
-		fmt.Println("Name : Attempts left : Word")
-		var sortedRanking [][3]string // PlayerName : Attempts left : Word
-		for i := 0; i < len(ranking); i++ {
-			if len(sortedRanking) == 0 {
-				sortedRanking = append(sortedRanking, ranking[i])
-			} else {
-				for j := 0; j < len(sortedRanking); j++ {
-					if stringToInt(ranking[i][1]) >
-						stringToInt(sortedRanking[j][1]) {
-						sortedRanking = append(sortedRanking[:j], append([][3]string{ranking[i]}, sortedRanking[j:]...)...)
-						break
-					} else if j == len(sortedRanking)-1 {
-						sortedRanking = append(sortedRanking, ranking[i])
-						break
-					}
-				}
-			}
-		}
-		for i := 0; i < len(sortedRanking); i++ {
-			if i < 10 {
-				if i == 0 {
-					fmt.Println("🥇 " + sortedRanking[i][0] + " with " + sortedRanking[i][1] + " attempts left and the word " + sortedRanking[i][2])
-				} else if i == 1 {
-					fmt.Println("🥈 " + sortedRanking[i][0] + " with " + sortedRanking[i][1] + " attempts left and the word " + sortedRanking[i][2])
-				} else if i == 2 {
-					fmt.Println("🥉 " + sortedRanking[i][0] + " with " + sortedRanking[i][1] + " attempts left and the word " + sortedRanking[i][2])
-				} else {
-					fmt.Println("🏅 " + sortedRanking[i][0] + " with " + sortedRanking[i][1] + " attempts left and the word " + sortedRanking[i][2])
-				}
-			}
-		}
-	}
-	fmt.Println("Do you want to continue ? (y/n)")
-	fmt.Scanln(&answer)
-	for answer != "y" && answer != "n" {
-		fmt.Println("Please enter a valid answer.")
-		fmt.Scanln(&answer)
-	}
-	if answer != "y" {
-		return
-	}
+}
 
-	if len(args) == 0 {
-		fmt.Println("Enter arguments")
-		return
-	} else if len(args) == 1 {
-		if core.Readable("assets/Dictionary/" + args[0]) {
-			core.NewGame(args, MyHangManData)
-		} else {
-			fmt.Println("1 - Please provide a valid argument.")
-		}
-	} else if present, pos := core.ContainsArray(args, "--startWith"); present {
-		if core.Readable("assets/Save/" + args[pos+1]) {
-			saved := core.ReadFile("assets/Save/" + args[2])
-			if len(saved) == 0 || saved[0] == "" {
-				core.NewGame(args, MyHangManData)
-				return
-			}
-			MyHangManData.ToFind = saved[0]
-			MyHangManData.Word = saved[1]
-			MyHangManData.PlayerName = saved[2]
-			MyHangManData.WordLength = len(MyHangManData.Word)
-			MyHangManData.Attempts = stringToInt(saved[3])
-			core.Play(core.HangManData(MyHangManData))
-			core.EmptyFile("assets/Save/" + args[2])
-		} else {
-			fmt.Println("2 - Please provide a valid argument.")
-		}
-	} else if present, pos := core.ContainsArray(args, "--letterFile"); present {
-		if core.Readable("assets/Ascii-letters/" + args[pos+1]) {
-			MyHangManData.IsAscii = true
-			MyHangManData.Ascii = core.ReadFile("assets/Ascii-letters/" + args[pos+1])
-			core.NewGame(args, MyHangManData)
-		} else {
-			fmt.Println("3 - Please provide a valid argument.")
-		}
+func handleLetterFile(args []string, pos int, MyHangManData core.HangManData) {
+	if core.Readable("assets/Ascii-letters/" + args[pos+1]) {
+		MyHangManData.IsAscii = true
+		MyHangManData.Ascii = core.ReadFile("assets/Ascii-letters/" + args[pos+1])
+		core.NewGame(args, MyHangManData)
+	} else {
+		fmt.Println("Please provide a valid argument.")
 	}
 }
